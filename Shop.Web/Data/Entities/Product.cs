00847d08b3d6a723ab2143f51cda5b8ddd22b2c2
Fabian Yesith Garcia Jurado <fygarciaj@users.noreply.github.com>
2020-01-12 22:44:26 -0500

    Changes in database

--- Shop.Web/Data/Entities/Product.cs
+++ Shop.Web/Data/Entities/Product.cs
@@ -12,6 +12,7 @@ namespace Shop.Web.Data.Entities
     {
         public int Id { get; set; }
 
+        [Required, MaxLength(50)]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -21,10 +22,10 @@ namespace Shop.Web.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Availabe?")]
         public bool IsAvailabe { get; set; }
